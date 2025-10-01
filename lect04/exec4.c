d7784dc41ae141f9d09edab9e5b11d276d0f89f6
hamsu <hamsj3808@gmail.com>
2025-10-01 01:44:46 +0000

    Add lect04 process codes

---
+++ lect04/exec4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int child, pid, status;
+    pid = fork();
+    if (pid == 0) { /* 자식 프로세스 */
+        execlp(argv[1], argv[1], NULL);
+        fprintf(stderr, "Error in execclp: %s\n", argv[1]);
+    } else { /* 부모 프로세스 */
+        child = wait(&status);
+        printf("child %d is terminated\n", pid);
+        printf("\twith status %d\n", status>>8);
+    }
+    return 0;
+}
+
